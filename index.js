a9e55e4474b77ee69beb5cab1981bc423c678b01
hossainshakil242 <hossainshakil242@gmail.com>
2023-10-16 10:51:33 +0600

    finish some introduction

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+Brendan Eich creator by javaScript 1995
+Official name is  ECMA Script 
+*/
+
+/* How type value in js
+* Fixed Value call Liteartor;
+* Variable value call variables;
+*/
+
+/* Scope 
+* global scope;
+* functional scope;
+* block scope;
+*/
+
+let x ; // call Declaration 
+x = 5; // call Assign 
